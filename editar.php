1be6aabf4b40199a5d55f08bcd8fade00c758ec5
beatrixsou <107127462+beatrixsou@users.noreply.github.com>
2022-11-29 18:30:30 -0500

    Add files via upload

--- editar.php
+++ editar.php
@@ -1,45 +1,192 @@
 <?php
-require 'config.php';
 
-$info = [];
-$id = filter_input(INPUT_GET, 'id');
-if($id) {
-    $sql = $pdo->prepare("SELECT * FROM usuarios WHERE id = :id");
-    $sql->bindValue(':eid', $id);
-    $sql->execute();
+    if(!empty($_GET['id']))
+    {
+        
+        include_once('config.php');
 
-    if($sql->rowCount() > 0){
+        $id = $_GET['id'];
 
-        $info = $sql->fetch( PDO::FETCH_ASSOC);
+        $sqlSelect = "SELECT * FROM usuarios WHERE id=$id";
 
-    }else{
-        header("Location: index.php");
-        exit;
-    }
+        $result = $conexao->query($sqlSelect);
 
-}else {
-    header("Location: index.php");
-    exit;
-}
+        if($result->num_rows > 0){
 
+            while($user_data = mysqli_fetch_assoc($result)){
+                $nome = $user_data['nome'];
+                $email = $user_data['email'];
+                $senha = $user_data['senha'];
+                $telefone = $user_data['telefone'];
+                $genero = $user_data['genero'];
+                $data_nascimento = $user_data['data_nascimento'];
+                $cidade = $user_data['cidade'];
+                $estado = $user_data['estado'];
+                $endereco = $user_data['endereco'];
+            }
+
+        }else{
+            header("Location: sistema.php");
+        }
+
+                
+    } else{
+        header("Location: sistema.php");
+    }  
+        
 ?>
 
-<h1>Editar Usuário</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            background-image: linear-gradient(45deg, cyan, yellow);
+        }
+        .box{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background-color: rgba(0, 0, 0, 0.6);
+            padding: 15px;
+            border-radius: 15px;
+            width: 20%;
+            color: white;
+        }
+        fieldset{
+            border: 3px solid dodgerblue;
+        }
+        legend{
+            border: 1px solid dodgerblue;
+            padding: 10px;
+            text-align: center;
+            background-color: dodgerblue;
+            border-radius: 8px;
+            color: white;
+        }
+        .inputBox{
+            position: relative;
+        }
+        .inputUser{
+            background: none;
+            border: none;
+            border-bottom: 1px solid white;
+            outline: none;
+            color: white;
+            font-size: 15px;
+            width: 100%;
+            letter-spacing: 2px;
 
-<form method= "POST" action="editar_action.php">
+        }
+        .labelInput{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            pointer-events: none;
+            transition: .5s;
 
-<input type="hidden" name="id" value="<?=$info['id'];?>" />
+        }
+        .inputUser:focus ~ .labelInput, 
+        .inputUser:valid ~ .labelInput{
+            top: -20px;
+            font-size: 12px;
+            color: dodgerblue;
+        }
+        #data_nascimento{
+            border: none;
+            padding: 8px;
+            border-radius: 10px;
+            outline: none;
+            font-size: 15px;
 
-<label>
-    Nome:<br/>
-    <input type="text" name="name" value="<?=$info['nome'];?>" />
-</label><br/><br/>
+        }
+        #update{
+            background-image: linear-gradient(to right, rgb(0, 92, 197), rgb(90, 20, 220));
+            width: 100%;
+            border: none;
+            padding: 15px;
+            color: white;
+            font-size: 15px;
+            cursor: pointer;
+            border-radius: 10px;
+        }
+        #update:hover{
+            background-image: linear-gradient(to right, rgb(0, 80, 172), rgb(80, 19, 195));
+        }
 
-<label>
-    E-mail:<br/>
-    <input type="email" name="email" value="<?=$info['email'];?>" />
-</label><br/><br/>
+    </style>
+</head>
+<body>
+    <a href="sistema.php">Voltar</a>
+    <div class="box">
+        <form action="salveEditar.php" method="POST">
+            <fieldset>
+                <legend><b>Formulário de Clientes</b></legend>
+                <br>
+                <div class="inputBox">
+                    <input type="text" name="nome" id="nome" class="inputUser" value="<?php echo $nome?>" required>
+                    <label for="nome" class="labelInput">Nome Completo</label> 
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="email" name="email" id="email" class="inputUser" value="<?php echo $email?>" required>
+                    <label for="email" class="labelInput">E-mail</label> 
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="password" name="senha" id="senha" class="inputUser" value="<?php echo $senha?>" required>
+                    <label for="senha" class="labelInput">Senha</label> 
+                </div>
+                <br><br>
+                
+                <div class="inputBox">
+                    <input type="tel" name="telefone" id="telefone" class="inputUser" value="<?php echo $telefone?>" required>
+                    <label for="telefone" class="labelInput">Telefone</label> 
+                </div>
+               
+                <p>Sexo</p>
+                <input type="radio" id="feminino" name="genero" value="feminino" value="<?php echo ($genero == 'feminino') ? 'checked' : '' ?>" required>
+                <label for="feminino">Feminino</label>    
+                <br>            
+                <input type="radio" id="masculino" name="genero" value="masculino" value="<?php echo ($genero == 'masculino') ? 'checked' : '' ?>" required>
+                <label for="masculino">Masculino</label>  
+                <br>             
+                <input type="radio" id="outro" name="genero" value="outro" value="<?php echo ($genero == 'outro') ? 'checked' : '' ?>" required>
+                <label for="outro">Outro</label>   
+                <br><br>             
+                
+                <label for="data_nascimento"><b>Ano Nascimento</b></label> 
+                <input type="date" name="data_nascimento" id="data_nascimento" value="<?php echo $data_nascimento?>" required>                    
+                <br><br>
 
-<input type="submit" value="Salvar" />
+                <div class="inputBox">
+                    <input type="text" name="cidade" id="cidade" class="inputUser" value="<?php echo $cidade?>" required>
+                    <label for="cidade" class="labelInput">Cidade</label> 
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="estado" id="estado" class="inputUser" value="<?php echo $estado?>" required>
+                    <label for="Estado" class="labelInput">Estado</label> 
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="endereco" id="endereco" class="inputUser" value="<?php echo $endereco?>" required>
+                    <label for="nome" class="labelInput">Endereço</label> 
+                </div>
+                <br><br>
+                <input type="hidden" name="id" value="<?php echo $id ?>" required>
 
-</form>
+                <input type="submit" name="update" id="update">
+                
+            </fieldset>
+
+        </form>
+    </div>
+</body>
+</html>
